9056fdb137123cd3213cbfab63a1c672ad4469fd
Mertcan Yigin <mcanyigin@gmail.com>
2016-11-15 19:11:31 +0300

    created empty project

---
+++ twitter-search/twitter-search/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  twitter-search
+//
+//  Created by mcan on 15/11/2016.
+//  Copyright © 2016 mcan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
