f4f9c40e241416d2b2f8b816b56dc43d10bfd3bb
Mertcan Yigin <mcanyigin@gmail.com>
2016-11-17 20:37:15 +0300

    added detail view file

---
+++ twitter-search/Views/DetailView/TweetDetailView.h
@@ -0,0 +1,13 @@
+//
+//  TweetDetailView.h
+//  twitter-search
+//
+//  Created by mcan on 17/11/2016.
+//  Copyright © 2016 mcan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetDetailView : UIView
+
+@end
---
+++ twitter-search/Views/DetailView/TweetDetailView.m
@@ -0,0 +1,13 @@
+//
+//  TweetDetailView.m
+//  twitter-search
+//
+//  Created by mcan on 17/11/2016.
+//  Copyright © 2016 mcan. All rights reserved.
+//
+
+#import "TweetDetailView.h"
+
+@implementation TweetDetailView
+
+@end
